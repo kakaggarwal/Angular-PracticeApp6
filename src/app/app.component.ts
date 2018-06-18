8a07debdbf87ea77427116a2bc6a20bd4bdfbee1
Krishan Aggarwal <kakaggarwal@gmail.com>
2018-06-18 20:45:02 +0530

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <img width="300" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://github.com/angular/angular-cli/wiki">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: 'app.component.html',
   styles: []
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'PracticeApp6';
+  firstName:string;
+  lastName:string;
+  status:boolean;
+  items: object[];
+  username:string;
+  password:string;
+  posts: object;
+
+  constructor (private http: HttpClient) {}
+
+  ngOnInit() {
+    this.firstName = "Jane";
+    this.lastName = "Doe";
+    this.status = false;
+    this.items = [
+      {name: "Krishan"},
+      {name: "Paras"},
+      {name: "Pranjal"},
+      {name: "Kritika"},
+      {name: "Guddu"},
+    ]
+    this.username = "";
+    this.password = "";
+    this.http.get("http://jsonplaceholder.typicode.com/posts").subscribe(res => {
+      this.posts = res; 
+    })
+  }
+
+  displayFirstName() {
+    return this.firstName;
+  }
+
+  greetUser() {
+    window.alert(`Hola ${this.firstName}, How you doing ?`);
+  }
+
+  activateUserStatus() {
+    this.status = true;
+  }
+
+  getOffMe() {
+    window.alert('get off me');
+  }
+
+  dontWrite() {
+    window.alert("dont write anything here !!");
+  }
+  
+  submitLogin(form: NgForm) {
+    console.log(form);
+    console.log(form.value);
+
+    if (form.value.username !== "" && form.value.password === "romeo") {
+      window.alert("Holla Amigos") 
+    }
+
+    this.http.post("http://jsonplaceholder.typicode.com/posts", {
+      userId:Math.random(),
+      id:Math.random(),
+      title:"lorem ipsum blorem pipsum",
+      body:"lorem's dad went shopping with his girlfriend and got caught by his wife and then happened dhulayi and dhulayi."
+    }).subscribe(res => { console.log(res); }, error => { console.log(error); })
+
+    // window.alert(`Hola ${form.username}, How you doing ?`);
+  }
 }
+ 
